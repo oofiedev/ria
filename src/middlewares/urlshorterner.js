fbb6719111560e2a0139285cfbb1bb66d72458c4
oofie <tdkmike7406@gmail.com>
2023-04-23 16:24:41 +0700

    Fixed gitignore

--- src/middlewares/urlshorterner.js
+++ src/middlewares/urlshorterner.js
@@ -17,19 +17,6 @@ app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 
-// Database connection
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     logger.warn(`Db Connected`);
-//   })
-//   .catch((err) => {
-//     logger.error(err);
-//   });
-
 // get all saved URLs
 router.get("/all", async (req, res) => {
   try {
